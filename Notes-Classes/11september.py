74836fbea35091082bb166305ece9c04be53b573
Atharva Patel <atharva.scripting@gmail.com>
2025-09-11 08:27:49 +0530

    Updated Notes

--- Notes-Classes/11september.py
+++ Notes-Classes/11september.py
@@ -6,8 +6,23 @@
 #Identity operator 
 """is & is not -- compares if obj is same or not --checks the memory allocation
    id()- memory address of a particular variable
-"""   
+   for small integers, float,strings.. python caches them in memory for efficency 
+   so in some cases == and is behaves same
 
 
-    
-    
+x=20
+y=30
+print(id(x),id(y)) #-- 10747024 10747344
+print(x==y)
+print (x is y)
+
+x=20
+y=20
+print(id(x),id(y)) #-- 10747024 10747024
+print(x==y)
+print (x is y)
+
+in above is and == was same because of small integer(5,256)
+
+
+"""

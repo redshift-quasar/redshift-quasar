9c5998b3a5bee2b22d25eb0cb09bdcd20f93bc82
Atharva Patel <atharva.scripting@gmail.com>
2025-09-11 08:16:53 +0530

    Added 11 sept notes

---
+++ Notes-Classes/11september.py
@@ -0,0 +1,13 @@
+#Polymorphic Operator
+"""Takes more than one form 
+    1. + it can be used for addition and concatination
+    2. * used for multiplication and replicate string --- Good * 3==GoodGoodGood"""
+
+#Identity operator 
+"""is & is not -- compares if obj is same or not --checks the memory allocation
+   id()- memory address of a particular variable
+"""   
+
+
+    
+    
--- Notes-Classes/9september.py
+++ Notes-Classes/9september.py
@@ -72,3 +72,6 @@ else:
       >> shift right
       ^  bitwise xor """   
  
+  
+ 
+ 

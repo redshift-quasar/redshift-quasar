96e9014e6567317da7b767bc165c971e45006152
Atharva Patel <atharva.scripting@gmail.com>
2025-09-20 09:15:47 +0530

    Delete Homeworks directory

--- Homeworks/camSales.py
+++
@@ -1,8 +0,0 @@
-BasicSalary=25000
-CamSold=int(input("Input the number of Cameras sold : "))
-CamPrice=int(input("Enter the Price of a Camera : "))
-Bonus=200*CamSold
-Commission=0.12*CamSold*CamPrice
-print("Bonus : ", Bonus)
-print("Commission : ", Commission)
-print("Gross Salary : ",BasicSalary+Bonus+Commission)
--- Homeworks/star.py
+++
@@ -1,8 +0,0 @@
-
-for i in range(0,5,2): 
-    print (" ")
-    for k in range(4,i,-2):
-        print(" ",end=" ")
-    for j in range(i+1):
-         print("* ",end="")
-      

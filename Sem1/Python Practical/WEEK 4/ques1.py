5e6b976cf54669338b9b000ee950ce6668e33543
Atharva Patel <atharva.scripting@gmail.com>
2025-09-25 08:44:03 +0530

    Updated Notes

--- Sem1/Python Practical/WEEK 4/ques1.py
+++ Sem1/Python Practical/WEEK 4/ques1.py
@@ -1,10 +1,10 @@
 n=int(input("Enter the Number: "))
 for i in range (1,n+1):
-    if i%3==0 and i%5!=0:
-        print("Buzz ")
-    elif i%5==0 and i%3!=0:
+    if i%3==0 and i%5==0:
+        print("FizzBuzz ")
+    elif i%5==0:
         print("Fizz")
-    elif i%3==0 and i%5==0:
-        print("BuzzFizz")
+    elif i%3==0:
+        print("Buzz")
     else:
         print(i)                

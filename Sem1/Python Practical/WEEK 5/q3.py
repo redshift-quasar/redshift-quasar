eab2b7e9efd7616256e16d0d69248db6185ab0fe
Atharva Patel <atharva.scripting@gmail.com>
2025-09-29 15:38:10 +0530

    Week 5 Practicals

--- Sem1/Python Practical/WEEK 5/q3.py
+++ Sem1/Python Practical/WEEK 5/q3.py
@@ -1,8 +1,14 @@
-arr1=[1,3,5,7,9]
+arr1=[1,3,5,7,9,11,13]
 arr2=[2,4,6,8,10]
+length=min(len(arr1),len(arr2))
 arr3=[]  #empty list to merge the both List
-
-for i in range(len(arr1)):
+count=0
+for i in range(length):
     arr3.append(arr1[i]) #adding from arr1
     arr3.append(arr2[i]) #adding from arr2
+    count=i
+if len(arr1)<len(arr2):           #Adding the remaining element to the list
+    arr3.extend(arr2[count+1:])
+else:
+    arr3.extend(arr1[count+1:])
 print(arr3)              #printing the merged List
--- Sem1/Python Practical/WEEK 5/q5.py
+++ Sem1/Python Practical/WEEK 5/q5.py
@@ -1,8 +1,12 @@
 nums=input("enter the numbers seperated with spaces : ").split()
 copy=[]
+if not nums:
+    print("Empty")
 for i in range (len(nums)-1):         #iterating through the loops with 1 less as comparing with next number that may give index error
             if nums[i]!=nums[i+1]:    #comparing current number with next number
                 copy.append(nums[i])  #using append to add the unique number
 copy.append(nums[len(nums)-1])        #including the last number always as it is unique in sorted array
 print(copy)
-        
+
+
+#Try without copy variable

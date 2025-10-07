ed50a1facd02dc7a940415b8a95dae7c8ea28d7d
Atharva Patel <atharva.scripting@gmail.com>
2025-10-07 11:05:16 +0530

    Strings

--- Sem1/Notes-Classes/Unit 2/6th October/ques6.py
+++ Sem1/Notes-Classes/Unit 2/6th October/ques6.py
@@ -5,12 +5,7 @@ But she said the butter bitter
 If I put it in my batter
 It will make my batter bitter
 But a bit of better butter
-That would make my batter better
-
-So Betty bought a bit of better butter
-And put it in her batter
-And the batter was not bitter
-So it was better Betty bought a bit of better butter"""
+That would make my batter better"""
 
 
 words=str1.lower().split()
